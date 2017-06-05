22043a75602e3b0213ed3181e667d19f0b5d4d3c
Quynh Nguyen <qn@rosslynanalytics.com>
2017-06-05 10:53:30 +0100

    Age in days: Done

--- Wagon-0/04. Age in days/age_in_days.rb
+++ Wagon-0/04. Age in days/age_in_days.rb
@@ -1,7 +1,14 @@
-def age_in_days(day, month, year)
-  # your code goes here !
-end
+require 'time'
 
+def age_in_days(day, month, year) 
+  dob = Date.parse("#{year}/#{month}/#{day}")
+  days = Date.today.mjd - dob.mjd
+
+  # Another way
+  # days = (Date.today - dob).to_i
+
+  "You have #{days} days buddy"
+end
 
 # Testing your code
 puts age_in_days(6, 11, 1985)
