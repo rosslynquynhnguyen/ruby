2add12a056160306622782922072a7c5ff3527e6
Quynh Nguyen <qn@rosslynanalytics.com>
2017-06-05 11:32:12 +0100

    What's your name: Done

--- Wagon-0/05. What's your name ?/introduce_yourself.rb
+++ Wagon-0/05. What's your name ?/introduce_yourself.rb
@@ -1,1 +1,16 @@
 # your code goes here, introduce yourself !
+print "Enter your first name: "
+first_name = gets.chomp
+print "Enter your middle name: "
+middle_name = gets.chomp
+print "Enter your last name: "
+last_name = gets.chomp
+
+full_name = first_name + ' ' + middle_name + ' ' + last_name
+
+puts "#Hello, #{full_name} !"
+puts "Your name has got #{full_name.length} characters, including spaces."
+
+if first_name.casecmp("quynh") == 0 && middle_name.casecmp("huu") == 0 && last_name.casecmp("nguyen") == 0
+    puts "You are a good person!"
+end 
