2d140b1f7620fd6655465054b4cb0c6e0b1ccfc2
Quynh Nguyen <qn@rosslynanalytics.com>
2017-06-05 21:50:49 +0100

    Code-style gym: done

--- Wagon-0/07. Code-style Gym/codestyle_gym.rb
+++ Wagon-0/07. Code-style Gym/codestyle_gym.rb
@@ -1,46 +1,55 @@
-class Player_moving
-  Grid_POSITIONS = (1..100).to_a
-  
-def initialize ( position ) 
-  @position = position ; @iterations = 0; 
-raise "Position must be between 1 and 100" unless Grid_POSITIONS.include? @position
-  end
-  
-      def move   distanceInMeters
-@position += distanceInMeters
-  end
-  
-def play
-  if(  self.status_Player==:play   ) 
-if rand(1..100) < 50 ; move(rand(1..10))
-  else
-        move(-rand(1..10))
-end
-  end
-end
+# This is a fantastic class which will do something amazing
+class PlayerMoving
+  # Intial upper case letter indicates a constant
+  GRID_POSITIONS = (1..100).to_a
 
-def cheat; @position = 101; end
-  
-def status_Player
-    if( @position >   100 )
-      :win 
-    elsif(@position<0) ; :loose
-    else;  :play
+  def initialize(position)
+    @position = position
+    @iterations = 0
+
+    raise 'Position must be between 1 and 100' unless GRID_POSITIONS.include? @position
+  end
+
+  def move(distance_in_meters)
+    @position += distance_in_meters
+  end
+
+  def play
+    return unless status_player == :play
+    if rand(1..100) < 50
+      move(rand(1..10))
+    else
+      move(-rand(1..10))
     end
   end
-  
-  def hasWon ; status_Player  == :win ; end
-    
+
+  def cheat
+    @position = 101
+  end
+
+  def status_player
+    if @position > 100
+      :win
+    elsif @position < 0
+      :loose
+    else
+      :play
+    end
+  end
+
+  def won?
+    status_player == :win
+  end
 end
 
-player = Player_moving.new(50)
+player = PlayerMoving.new(50)
 player.play
-puts player.status_Player == :play # true
+puts player.status_player == :play # true
 player.cheat
-puts player.hasWon == true
-  
+puts player.won? == true
+
 begin
-  Player_moving.new(110)
+  PlayerMoving.new(110)
 rescue RuntimeError => e
-  puts e.to_s == "Position must be between 1 and 100" # true
+  puts e.to_s == 'Position must be between 1 and 100' # true
 end
