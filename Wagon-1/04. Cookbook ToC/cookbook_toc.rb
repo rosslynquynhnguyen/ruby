85f30fbc65f4305f1648a6e85553bfdfa5167433
Quynh Nguyen <qn@rosslynanalytics.com>
2017-06-06 09:06:32 +0100

    Cookbook ToC: Done

--- Wagon-1/04. Cookbook ToC/cookbook_toc.rb
+++ Wagon-1/04. Cookbook ToC/cookbook_toc.rb
@@ -9,3 +9,8 @@ chapters = [
            ]
            
 # You program goes here !
+puts title.center(65)
+puts ''
+for i in 0..chapters.length - 1
+  puts "Chapter #{(i + 1).to_s.rjust(2)} : #{chapters[i][0].ljust(40)} #{('page ' + chapters[i][1].to_s).rjust(10)}"
+end
