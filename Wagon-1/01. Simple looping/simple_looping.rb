65dec59707ee54acee69caee1847c8a9c7446bde
Quynh Nguyen <qn@rosslynanalytics.com>
2017-06-06 01:48:35 +0100

    Simple looping: Done

--- Wagon-1/01. Simple looping/simple_looping.rb
+++ Wagon-1/01. Simple looping/simple_looping.rb
@@ -1,12 +1,34 @@
-# The objective is to create a function that computes the sum of the integers from a min value to a max value
-
-def sum(min,max)
+# The objective is to create a function that computes
+# the sum of the integers from a min value to a max value
+def sum_using_for(min, max)
   # your code here
+  total = 0
+  for i in min..max
+    total += i
+  end
+  total
+end
+
+def sum_using_each(min, max)
+  total = 0
+  (min..max).each do |i|
+    total += i
+  end
+  total
+end
+
+def sum_recursive(min, max)
+  if max == min
+    max
+  else
+    max + sum_recursive(min, max - 1)
+  end
 end
 
 # Testing your code
-
 min = 1
 max = 100
-sum = sum(1,100)
-puts sum == 5050 # => true
+sum = 5050
+puts sum_using_for(min, max) == sum
+puts sum_using_each(min, max) == sum
+puts sum_recursive(min, max) == sum
