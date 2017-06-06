d076ab668cc052514aa7f08b2b11cc4afaf57d50
Quynh Nguyen <qn@rosslynanalytics.com>
2017-06-06 08:29:20 +0100

    Beer song: Done

--- Wagon-1/03. Beer song /beer_song.rb
+++ Wagon-1/03. Beer song /beer_song.rb
@@ -1,8 +1,16 @@
+# your code goes here
 def beersong(nb_at_start)
-  # your code goes here
+  if nb_at_start == 1
+    puts '1 bottle of beer on the wall, 1 bottle of beer!'
+    puts 'Take one down, pass it around, no more bottles of beer on the wall!'
+  elsif nb_at_start > 1
+    puts "#{nb_at_start} bottles of beer on the wall, #{nb_at_start} bottles of beer!"
+    puts "Take one down, pass it around, #{nb_at_start - 1} #{nb_at_start -1 > 1?'bottles':'bottle'} of beer on the wall!"
+    beersong(nb_at_start - 1)
+  end
 end
 
-beersong( ARGV[0] )
+beersong(ARGV[0].to_i)
 
 # output (if called with 5 beers) :
 
