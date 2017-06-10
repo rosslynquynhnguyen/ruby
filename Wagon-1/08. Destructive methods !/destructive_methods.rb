377eb92fad8a8dce106e5499b6617e75867ad20f
Quynh Nguyen <qn@rosslynanalytics.com>
2017-06-10 07:51:23 +0100

    Destructive_methods: Done

--- Wagon-1/08. Destructive methods !/destructive_methods.rb
+++ Wagon-1/08. Destructive methods !/destructive_methods.rb
@@ -1,2 +1,13 @@
 # Find an example which illustrates the difference between chosen_method and chosen_method!
 # Copy / Paste this example here !
+a = (1..30).to_a
+
+nine_times = a.select{ |foo| foo % 9 == 0 }
+
+puts nine_times == [9, 18, 27]
+puts a == (1..30).to_a
+
+eight_times = a.select!{ |foo| foo % 8 == 0 }
+
+puts eight_times == [8, 16, 24]
+puts a == [8, 16, 24]
