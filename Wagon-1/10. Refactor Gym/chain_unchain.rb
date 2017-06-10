d9c08d31ec477ef8ba6118a1a1240be844f62b7a
Quynh Nguyen <qn@rosslynanalytics.com>
2017-06-10 11:26:23 +0100

    Refactor gym: Done

--- Wagon-1/10. Refactor Gym/chain_unchain.rb
+++ Wagon-1/10. Refactor Gym/chain_unchain.rb
@@ -1,20 +1,20 @@
+# Ruby fonction to play to "Des chiffres et des lettres.."
 def shuffle_word(a_word)
-  # Ruby fonction to play to "Des chiffres et des lettres.."
-  
-  cap_word = a_word.upcase
-  chars_enum = cap_word.chars
-  array = chars_enum.to_a
-  shuffle_array = array.shuffle
-  
-  return shuffle_array
-  
+  a_word.upcase.chars.shuffle
 end
-
 
 def quote_prime_numbers(n)
-  (1..n).find_all {|i| (2..i-1).select {|k| i % k == 0 }.count == 0 }.map{ |prime_num| "#{prime_num} is prime"}
+  (2..n).find_all {|i| (2..i-1).select {|k| i % k == 0 }.count == 0 }.map{ |prime_num| "#{prime_num} is prime"}
 end
 
+def quote_prime_numbers_readable(n)
+  primes = Array.new
+  (2..n).each do |i|
+    primes.push "#{i} is prime" if (2..i-1).select { |j| i % j == 0 }.count == 0
+  end
+  primes
+end
 
 puts shuffle_word("nabuchodonosor")
-puts quote_prime_numbers(30)
+puts quote_prime_numbers(30) == quote_prime_numbers_readable(30)
+puts quote_prime_numbers_readable(30)
