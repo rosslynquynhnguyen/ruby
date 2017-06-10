232c0e37dbfae0d73310999a8fd5d7e63c853dd5
Quynh Nguyen <qn@rosslynanalytics.com>
2017-06-10 10:25:58 +0100

    Debug Gym: Done

--- Wagon-1/09. Debug Gym/bugged_concatenate.rb
+++ Wagon-1/09. Debug Gym/bugged_concatenate.rb
@@ -1,8 +1,15 @@
 def bugged_concatenate(array)
   # 2. fix the code: TypeError
-  puts array.inject("") { |output, element| output + element }.upcase
+  puts array.inject("") { |output, element| output + element.to_s }.upcase
 end
 
-
 # 1. fix the method call: ArgumentError
-bugged_concatenate( 1, "9", 84, " ", "George Orwell" )
+bugged_concatenate([1, "9", 84, " ", "George Orwell"])
+
+# simple function responding diffently to Integer and String
+def multiply(source, times)
+  source * times
+end
+
+puts "3 x 5 = #{multiply(3,5)}"
+puts "Hello world! x 3 = " + multiply("Hello world! ", 3)
