6c69cc7fc954cdb11035d78f7af0156062549748
Quynh Nguyen <qn@rosslynanalytics.com>
2017-06-12 21:42:37 +0100

    Group anagrams: Done

--- Wagon-2/04. Combine anagrams/group_anagrams.rb
+++ Wagon-2/04. Combine anagrams/group_anagrams.rb
@@ -1,9 +1,17 @@
+# group_anagrams.rb
 def group_anagrams(words)
   # your code here
+  words.map{|word| [word.downcase.chars.sort, word]}
+    .group_by{|array, word| array}
+    .map{|key, val| [key, val.map{|array, word| word}]}
+    .map{|key, val| val}
+    .sort
 end
 
 # input:
-group_anagrams( ['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'scream'] )
+puts group_anagrams(['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'scream']) == [["cars", "racs", "scar"], ["four"], ["for"], ["potatoes"], ["creams", "scream"]].sort
+
+p group_anagrams(['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'scream'])
 
 # output:
 # =>  [["cars", "racs", "scar"], ["four"], ["for"], ["potatoes"], ["creams", "scream"]]
