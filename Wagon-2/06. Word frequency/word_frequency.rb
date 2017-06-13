5e202ef73a7656b22531ef90899aec80afd89b4b
Quynh Nguyen <qn@rosslynanalytics.com>
2017-06-13 08:45:20 +0100

    Word frequency: Done

--- Wagon-2/06. Word frequency/word_frequency.rb
+++ Wagon-2/06. Word frequency/word_frequency.rb
@@ -1,1 +1,19 @@
-# the file is yours !
+# word_frequency.rb
+
+require 'set'
+file_name = 'source-text.txt'
+
+top = 100
+content = File.read(file_name)
+
+# load stopwords
+stopwords = File.read('english-stopwords.txt').split(/\W+/).to_set
+
+words = content.split(/\W+/) #split by all non-word params, combined multiple delimeters
+            .group_by{|word| word.downcase} #group by downcase of the words
+            .map{|k,v| [k, v.count]} # project into word - count
+            .select{|k,v| !stopwords.include?(k)}
+            .sort_by{|k, v| v}
+            .reverse!         
+            .take(top)
+            .map{|k, v| puts "#{('\'' + k + '\'').ljust(15)}: #{v} occurrences"}
