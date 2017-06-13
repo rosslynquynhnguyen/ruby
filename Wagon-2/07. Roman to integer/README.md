019a41fe272dea6d41fe5738026d7b4a7da9edfb
Quynh Nguyen <qn@rosslynanalytics.com>
2017-06-13 12:12:16 +0100

    ReadMe updated
    And unit tests as well.

--- Wagon-2/07. Roman to integer/README.md
+++ Wagon-2/07. Roman to integer/README.md
@@ -2,3 +2,13 @@ Write a methods `roman_to_integer(roman_string)` which translates a roman string
 
 ## Tips
 Which data structure seems the most appropriate to map roman individual characters to integers ?
+
+## Hint
+For reference, these are the values of the letters used:
+* I = 1
+* V = 5
+* X = 10
+* L = 50
+* C = 100
+* D = 500
+* M = 1000
--- Wagon-2/07. Roman to integer/roman_to_integer.rb
+++ Wagon-2/07. Roman to integer/roman_to_integer.rb
@@ -1,9 +1,20 @@
+# roman_numerals.rb
 def roman_to_integer(roman_string)
-  # your code goes here
+  1
 end
 
 # testing your program
 puts roman_to_integer('IV') == 4 # => true
 puts roman_to_integer('XII') == 12 # => true
 puts roman_to_integer('XIX') == 19 # => true
-
+puts roman_to_integer('VI') = 6
+puts roman_to_integer('VIII') == 8
+puts roman_to_integer('IX') == 9
+puts roman_to_integer('XXI') == 21
+puts roman_to_integer('XXV') == 25
+puts roman_to_integer('LXXX') == 80
+puts roman_to_integer('MCMLXXIX') == 1979
+puts roman_to_integer('MCMXCIX') == 1999
+puts roman_to_integer('MMMMMV') == 5005
+puts roman_to_integer('MMCMXVIII') == 2918
+puts roman_to_integer('DCCLXXXV') == 785
