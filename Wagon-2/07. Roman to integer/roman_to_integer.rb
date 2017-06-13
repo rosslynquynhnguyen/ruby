4bdc9c112726f94c1b1be49952e997099f9f185a
Quynh Nguyen <qn@rosslynanalytics.com>
2017-06-13 16:15:21 +0100

    Roman to integer: Done

--- Wagon-2/07. Roman to integer/roman_to_integer.rb
+++ Wagon-2/07. Roman to integer/roman_to_integer.rb
@@ -1,13 +1,37 @@
 # roman_numerals.rb
 def roman_to_integer(roman_string)
-  1
+  roman_letters = { 
+    'I' => 1,
+    'V' => 5,
+    'X' => 10,
+    'L' => 50,
+    'C' => 100,
+    'D' => 500,
+    'M' => 1000
+  }
+
+  # array of current values
+  vals = roman_string.chars.map{|letter| roman_letters[letter]}
+  
+  sum = 0
+  for i in 0..vals.length - 1
+    current_val = vals[i]
+    next_val = if i == (vals.length - 1) then 0 else vals[i + 1] end
+
+    if current_val >= next_val 
+      sum += current_val
+    else
+      sum -= current_val
+    end
+  end
+
+  sum
 end
 
-# testing your program
 puts roman_to_integer('IV') == 4 # => true
 puts roman_to_integer('XII') == 12 # => true
 puts roman_to_integer('XIX') == 19 # => true
-puts roman_to_integer('VI') = 6
+puts roman_to_integer('VI') == 6
 puts roman_to_integer('VIII') == 8
 puts roman_to_integer('IX') == 9
 puts roman_to_integer('XXI') == 21
