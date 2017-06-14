9e47dd2b388a919ed83c2005a2d6e1935ff442a0
Quynh Nguyen <qn@rosslynanalytics.com>
2017-06-14 10:12:19 +0100

    Refactor: Done

--- Wagon-2/11. Refactor Gym/refactor_gym_tonic.rb
+++ Wagon-2/11. Refactor Gym/refactor_gym_tonic.rb
@@ -1,18 +1,11 @@
 # Very dirty code to make some dirty gym...
 def hop_hop_hop(number_of_exercises)
   for i in (1..number_of_exercises) do 
-    counter = 0
-    until counter == i do
+    (1..i).each {
       print "hop! "
-      counter += 1
-    end
-    unless counter != i # Test if reaching the end of the current exercise.
-      # Prints message in that case    
-      print 'Encore une fois..' + "\n"
-    end  
-    # Reset counter to 0 for the next exercise
-    counter = 0
+    }
+    print "Encore une fois..\n"
   end
 end
 
-hop_hop_hop(6)
+hop_hop_hop(10)
